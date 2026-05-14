334473bf9ba984e084a9cb997a7b99b254bbaa67
Minura Punchihewa <minurapunchihewa17@gmail.com>
2026-05-14 13:10:21 -0700

    associated convs with projects

--- alembic/versions/93375a6617f4_init.py
+++ alembic/versions/93375a6617f4_init.py
@@ -35,8 +35,10 @@ def upgrade() -> None:
         "conversations",
         sa.Column("id", sa.Uuid(), nullable=False),
         sa.Column("topic", sa.String(255), nullable=False),
+        sa.Column("project_id", sa.Uuid(), nullable=False),
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.text("CURRENT_TIMESTAMP"), nullable=True),
         sa.Column("modified_at", sa.DateTime(timezone=True), server_default=sa.text("CURRENT_TIMESTAMP"), nullable=True),
+        sa.ForeignKeyConstraint(["project_id"], ["projects.id"]),
         sa.PrimaryKeyConstraint("id"),
     )
 
--- cowork/models/conversation.py
+++ cowork/models/conversation.py
@@ -7,12 +7,15 @@ from cowork.models.base import BaseSQLModel
 
 if TYPE_CHECKING:
     from cowork.models.message import Message
+    from cowork.models.project import Project
 
 
 class Conversation(BaseSQLModel, table=True):
     __tablename__ = "conversations"
 
     topic: str = Field(description="Topic of the conversation", max_length=255)
+    project_id: UUID = Field(foreign_key="projects.id", description="Project this conversation belongs to")
 
+    project: "Project" = Relationship()
     messages: list["Message"] = Relationship()
 
