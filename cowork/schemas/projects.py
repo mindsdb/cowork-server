63b341eca572a1977f5a7183968a89ac6225bbf8
Minura Punchihewa <minurapunchihewa17@gmail.com>
2026-06-19 15:01:14 -0700

    enabled the path to be defined in creating project

--- cowork/schemas/projects.py
+++ cowork/schemas/projects.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel
+from pathlib import Path
+from pydantic import field_validator
 
 from cowork.schemas.base import CamelRequest
 
 
 class ProjectCreateRequest(CamelRequest):
     name: str
+    path: Path | None = None
+
+    @field_validator("path")
+    @classmethod
+    def validate_path(cls, path: str | None) -> Path | None:
+        return Path(path) if path else None
 
 
 class ProjectUpdateRequest(CamelRequest):
