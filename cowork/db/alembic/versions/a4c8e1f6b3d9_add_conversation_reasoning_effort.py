0497cee25b733ab6f02934d3fe8f5b1a79be9622
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2026-09-02 16:34:44 +0200

    Fix migration chain: make a4c8e1f6b3d9 revise e5b8d3f0a2c7 to maintain DAG

--- cowork/db/alembic/versions/a4c8e1f6b3d9_add_conversation_reasoning_effort.py
+++ cowork/db/alembic/versions/a4c8e1f6b3d9_add_conversation_reasoning_effort.py
@@ -1,7 +1,7 @@
 """add conversation reasoning_effort
 
 Revision ID: a4c8e1f6b3d9
-Revises: f1a3c9d7e2b5
+Revises: e5b8d3f0a2c7
 Create Date: 2026-08-27 00:00:00.000000
 
 """
@@ -13,11 +13,14 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "a4c8e1f6b3d9"
-# This migration landed on staging with f1a3c9d7e2b5 as its parent and is already
-# in the DB history. Keep the original parent to avoid breaking the migration chain
-# for DBs that already have it applied. The channel org-scoping migrations
-# (d4a7c2e9f1b3, e5b8d3f0a2c7) run BEFORE this one and don't conflict.
-down_revision: Union[str, Sequence[str], None] = "f1a3c9d7e2b5"
+# Originally this revises f1a3c9d7e2b5 (landed on staging 2026-08-27), but the
+# channel org-scoping migrations (d4a7c2e9f1b3 2026-08-17, e5b8d3f0a2c7
+# 2026-08-17) have been rebased to run before it. This migration now revises
+# e5b8d3f0a2c7 to maintain the migration DAG and match chronological order.
+# On staging DBs that have a4c8e1f6b3d9 applied with f1a3c9d7e2b5 parent,
+# Alembic will auto-merge the new migrations first (they're ancestors of an
+# already-applied revision) before reheading to e5b8d3f0a2c7 as the final head.
+down_revision: Union[str, Sequence[str], None] = "e5b8d3f0a2c7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
